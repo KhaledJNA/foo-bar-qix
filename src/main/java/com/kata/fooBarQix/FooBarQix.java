8d9a78f7fee93e5861df64ffdd6bcaa6a8502aab
Khaled Ben Taleb <bentaleb.khaled@gmail.com>
2020-01-16 16:35:07 +0100

    addition of : -The first test case which must return FooFoo and which fails. -Simple class FooBarQix with empty process method

---
+++ src/main/java/com/kata/fooBarQix/FooBarQix.java
@@ -0,0 +1,10 @@
+package com.kata.fooBarQix;
+
+public class FooBarQix {
+
+	public String process(int value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/test/java/com/kata/fooBarQix/FooBarQixTest.java
+++ src/test/java/com/kata/fooBarQix/FooBarQixTest.java
@@ -1,13 +1,35 @@
 package com.kata.fooBarQix;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
 /**
  * @author khaledbentaleb
  * Unit test for simple FooBarQixTest.
  */
 public class FooBarQixTest
 {
+	private static FooBarQix fooBarQix ;
+
 	
+	@BeforeAll
+	public static void init() {
+		fooBarQix = new FooBarQix();
+	}
 	
+	@Test
+	public void shouldReturnFooFoo() {
+		int value = 3;
+		String expected = "FooFoo";
+
+		String actual = fooBarQix.process(value);
+
+		assertEquals(expected, actual);
+
+	}
 	
 	
 }
